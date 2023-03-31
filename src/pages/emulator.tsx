98cfbac98f564eae35c1ada4a7508a69d0828094
LimeGradient <kfb13@scasd.org>
2023-03-31 14:09:59 -0400

    FEAT: Content Moved

---
+++ src/pages/emulator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const desmond = require("desmond-emu");
+
+export default class Emulator extends React.Component {
+    render() {
+        return (
+            <div className="emulator">
+                
+            </div>
+        );
+    }
+}
---
+++ src/pages/projects.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class CodingProjects extends React.Component {
+    render() {
+        return (
+            <div className='projects'>
+              <h1>Coding Projects</h1>
+              <h3>Projects I have made being shown here</h3>
+              <br />
+              <h1>Math.c</h1>
+              <p>Math.c is a custom built Math Library for C. I am working on it to
+                improve my Math skills and C skills. <a href='https://github.com/LimeGradient/math.c'>
+                  You can find the Github Repo here.
+                </a>
+              </p>
+            </div>
+          );
+    }
+}
