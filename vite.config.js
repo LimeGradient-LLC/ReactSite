0b88b71005dda9b1394c171942f3480795eac1ae
LimeGradient <kfb13@scasd.org>
2023-05-26 07:50:48 -0400

    fixxy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+var nWasmPath = __dirname + "N64Wasm/dist/";
+
 export default defineConfig({
   build: {
     rollupOptions: {
@@ -9,6 +11,9 @@ export default defineConfig({
         nes: resolve(__dirname, 'nes.html'),
         nes_embed: resolve(__dirname, 'nes-embed.js'),
         n64: resolve(__dirname, "N64Wasm/dist/index.html"),
+        n64_settings: resolve(__dirname, "N64Wasm/dist/settings.js"),
+        n64_roms: resolve(__dirname, "N64Wasm/dist/romlist.js"),
+        n64_script: resolve(__dirname, "N64Wasm/dist/script.js"),
       },
     },
   },
