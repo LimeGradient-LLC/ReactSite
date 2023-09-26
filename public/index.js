a54de57e8ddfb9cf23729e72252ce4e252fa899d
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2023-09-26 12:30:19 -0400

    WIP: Added More Songs

--- public/index.js
+++ public/index.js
@@ -5,27 +5,6 @@ let audioPlayerButtons;
 let songPlayer;
 const jsmediatags = window.jsmediatags
 
-window.onload = () => {
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
-
-    if (!accessToken) {
-        console.log('erorr')
-    }
-
-    fetch('https://discord.com/api/users/@me', {
-        headers: {
-            authorization: `${tokenType} ${accessToken}`,
-        },
-    })
-        .then(result => result.json())
-        .then(response => {
-            const { username, discriminator } = response;
-            console.log(username)
-        })
-        .catch(console.error);
-};
-
 window.addEventListener("DOMContentLoaded", () => {
     /*
       Set the Album Cover here because it
