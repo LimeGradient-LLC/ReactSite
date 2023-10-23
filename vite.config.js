73735cf917d9badfa31c613af9a5de19b626b253
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2023-10-23 13:02:13 -0400

    api

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        discordauth: resolve(__dirname, 'discordauth.html')
       },
     },
   },
