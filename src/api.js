04bb0ff6b54950a1db1da1fc955c464e6b898bf9
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-02-12 12:28:30 -0500

    new proxy

--- src/api.js
+++ src/api.js
@@ -19,19 +19,10 @@ router.get("/secure", (req, res) => {
 
 const proxyMiddleware = createProxyMiddleware({
   target: "https://discord.com/",
-  onProxyReq: fixRequestBody,
-  logLevel: 'debug',
   changeOrigin: true,
-  secure: false,
-  xfwd: true,
-  ws: true,
-  hostRewrite: true,
-  cookieDomainRewrite: true,
-  headers: {
-    "Connection": "keep-alive",
-    "Content-Type": "text/xml;charset=UTF-8",
-    "Accept": "*/*"
-  },
+  pathRewrite: {
+    "^/proxy": "",
+  }
 })
 
 router.get("/proxy", proxyMiddleware)
