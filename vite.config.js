9f3de27f93749a9ccc6d412597bb91cf43022adb
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-09-10 09:20:55 -0400

    mc + vite fix

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        gb: resolve(__dirname, "gb.html"),
-        n64: resolve(__dirname, "N64Wasm/dist/index.html"),
-        gba: resolve(__dirname, "gba.html"),
-        nds: resolve(__dirname, "ds.html"),
-        dos: resolve(__dirname, "dos.html")
       },
     },
   },
